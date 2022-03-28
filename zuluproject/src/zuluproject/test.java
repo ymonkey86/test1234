65de7fe52c2c7406c75cd9b723e41e293d550b04
ymonk <ymonk@local.wizvera.com>
2022-03-28 15:37:31 +0900

    1

---
+++ zuluproject/src/zuluproject/test.java
@@ -0,0 +1,10 @@
+package zuluproject;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("zul213213u");
+	}
+
+}
---
+++ zuluproject/src/zuluproject/test1234444.java
@@ -0,0 +1,10 @@
+package zuluproject;
+
+public class test1234444 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("zul213213213u");
+	}
+
+}
